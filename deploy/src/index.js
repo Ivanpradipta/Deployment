159c3fbe2526c38fefa4fcbb150ae44d389fb97a
unknown <ivan.pradipta@ti.ukdw.ac.id>
2023-04-17 22:41:06 +0700

    deploy

--- deploy/src/index.js
+++ deploy/src/index.js
@@ -2,15 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { ApolloProvider } from "@apollo/client";
 import reportWebVitals from './reportWebVitals';
-import client from './graphql';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ApolloProvider client={client}>
+  <React.StrictMode>
     <App />
-  </ApolloProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
